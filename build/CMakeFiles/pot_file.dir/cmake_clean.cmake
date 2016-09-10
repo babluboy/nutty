a35f0407d146815c972046953848379b7f6dba9f
Siddhartha Das <bablu.boy@gmail.com>
2016-09-10 22:00:57 +0100

    Removed Build Directory

--- build/CMakeFiles/pot_file.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/pot_file"
-  "po/nutty.pot"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/pot_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
