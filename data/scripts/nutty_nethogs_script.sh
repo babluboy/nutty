613fdb775c587b6030efae0276a11b7210a63e15
Siddhartha Das <bablu.boy@gmail.com>
2018-04-13 22:03:53 +0100

    Initial set up for nutty website

--- data/scripts/nutty_nethogs_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo nethogs $1 -t > /tmp/nutty_nethogs.txt &
-sleep 10
-sudo killall nethogs
-cat /tmp/nutty_nethogs.txt
-sudo rm /tmp/nutty_nethogs.txt
--- data/scripts/nutty_nmap_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo nmap -sn -oX /tmp/nutty_nmap.xml $1
-cat /tmp/nutty_nmap.xml
-sudo rm /tmp/nutty_nmap.xml
